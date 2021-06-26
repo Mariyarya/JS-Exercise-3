26fe502a63c4c55885b6b3ba0f446cc945321bbc
Mariyarya <78618476+Mariyarya@users.noreply.github.com>
2021-06-27 01:30:06 +0300

    JS Exercise 3
    Magic Ball

---
+++ app.js
@@ -0,0 +1,44 @@
+
+const userName = '';
+
+'Rya' ? console.log('Hello, Jane!') :
+    console.log('Hello!');
+
+const userQuestion = 'What is the question darling?';
+
+
+console.log(`userQuestion ${userName}`);
+
+
+const randomNumber = Math.floor(Math.random() * 8);
+
+switch (eightBall) {
+    case 0:
+        eightBall = 'It is certain';
+        brake;
+    case 1:
+        eightBall = 'It is decidedly so';
+        brake;
+    case 2:
+        eightBall = 'Reply hazy try again';
+        brake;
+    case 3:
+        eightBall = 'Cannot predict now';
+        brake;
+    case 4:
+        eightBall = 'Do not count on it';
+        brake;
+    case 5:
+        eightBall = 'My sources say no';
+        brake;
+    case 6:
+        eightBall = 'Outlook not so good';
+        brake;
+    case 7:
+        eightBall = 'Signs point to yes';
+        brake;
+};
+
+console.log(`Answer is: ${eightBall}`);
+const eightBall = '';
+console.log(eightBall);
